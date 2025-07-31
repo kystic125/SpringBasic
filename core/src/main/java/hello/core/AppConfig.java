01f1e1f5622c720ff0617b83065a818b64a7709f
. <32423432424a@a.22222222222222222>
2025-07-31 19:33:34 +0900

    객체 지향 원리 적용 (관심사 분리, 스프링으로 전환)

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,41 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// AppConfig: 구체 클래스를 선택한다
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+        // 생성자 주입
+    }
+
+    // 나중에 메모리멤버리파지토리->DB로 바꾸게 되면 해당 부분만 바꾸면 됨
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // 나중에 정액할인->정률할인으로 바꾸게 되면 여기만 바꾸면 됨
+    @Bean
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
+}
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,29 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 고정할인 -> 정률할인 변경을 위해선 OrderServiceImpl 의 해당 부분을 변경해야 한다
+    // DIP 위반 (인터페이스(추상)에만 의존하지 않고 구체 클래스에도 함께 의존하기 때문)
+    // 따라서 FixDiscount -> RateDiscount 변경 시 코드의 변경이 필요함 (OCP 위반)
+    // 이 문제를 어떻게 해결할까? (-> 아래처럼 인터페이스에만 의존하도록 설계 변경)
+    private final DiscountPolicy discountPolicy;
+    // 그러나 위 방법처럼 하면 구현체가 없는데 어떻게 실행될까...? (지금은 NullPointException 발생)
+    // -> 누군가가 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해주면 어떨까?
+    // AppConfig는 생성한 객체의 참조를 "생성자"를 통해 주입해준다.
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
