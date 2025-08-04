3564166fe9603083d61e42e509e3037fd414d232
. <32423432424a@a.22222222222222222>
2025-08-04 20:04:55 +0900

    컴포넌트 스캔 - 의존관계 자동 주입 및 탐색 위치와 기본 스캔 대상

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,17 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+//        basePackages = "hello.core.member", // 탐색할 패키지의 시작 위치 지정 가능 -> hello.core.member 하위만 탐색
+//        basePackageClasses = AutoAppConfig.class, // 내 패키지 (hello.core)부터 탐색
+        // 지정하지 않을 시 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치
+        // -> AppConfig 를 프로젝트 최상단에 두면 안적어도 됨!
+        // AppConfig 를 제외하기 위해 작성 (기존의 AppConfig 유지한 채 하기 때문에..)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
