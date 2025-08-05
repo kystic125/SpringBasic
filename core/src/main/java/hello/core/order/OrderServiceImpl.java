5ec5695a0f1594712e5034b03db278fdb1b8e500
. <32423432424a@a.22222222222222222>
2025-08-05 15:48:29 +0900

    다양한 의존관계 주입 방법

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,6 +6,7 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,6 +23,7 @@ public class OrderServiceImpl implements OrderService {
     // 그러나 위 방법처럼 하면 구현체가 없는데 어떻게 실행될까...? (지금은 NullPointException 발생)
     // -> 누군가가 OrderServiceImpl에 DiscountPolicy의 구현 객체를 대신 생성하고 주입해주면 어떨까?
     // AppConfig는 생성한 객체의 참조를 "생성자"를 통해 주입해준다.
+
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
@@ -40,4 +42,38 @@ public class OrderServiceImpl implements OrderService {
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
+
+    // 다양한 의존관계 주입 방법
+    // (final을 지우고 해야 실행됨!)
+
+    // 필드 주입 (비권장 -> 값을 바꿀 수 있는 방법이 없음 -> 별도의 setter를 열어 줘야 함)
+/*
+     @Autowired private MemberRepository memberRepository;
+     @Autowired private DiscountPolicy discountPolicy;
+     처럼 할 수도 있음
+*/
+
+    // 생성자 주입 (이 경우 위에 final을 없애야 함)
+/*
+   // @Autowired가 없으면 생성자로 자동 연결 // 이게 있으면 생성자가 없어도 됨
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+*/
+
+    // 일반 메서드 주입
+/*
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy, discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    처럼 사용 가능 (일반 메소드 위에 @Autowired를 작성한 형태)
+ */
 }
